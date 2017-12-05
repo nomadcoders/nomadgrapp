eb3f08df9fc58df8a76dae69eec26a15ee34e465
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-12-05 14:48:53 +0700

    #4-1 Preloading Assets

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppLoading, Asset, Font } from "expo";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
 
-export default class App extends React.Component {
+class App extends React.Component {
+  state = {
+    isLoadingComplete: false
+  };
   render() {
+    const { isLoadingComplete } = this.state;
+    if (!isLoadingComplete) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    }
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
+  _loadAssetsAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require("./assets/images/logo.png"),
+        require("./assets/images/logo-white.png"),
+        require("./assets/images/noPhoto.jpg"),
+        require("./assets/images/photoPlaceholder.png")
+      ]),
+      Font.loadAsync({
+        ...Ionicons.font,
+        ...MaterialIcons.font
+      })
+    ]);
+  };
+  _handleLoadingError = error => {
+    console.error(error);
+  };
+  _handleFinishLoading = async () => {
+    this.setState({
+      isLoadingComplete: true
+    });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
+export default App;
