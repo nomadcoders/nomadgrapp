23482cfc22ccbde08817bf4dfb36ed28eae1be34
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-12-05 21:50:14 +0700

    #4-8 LoginScreen part Two

--- screens/LogInScreen/presenter.js
+++ screens/LogInScreen/presenter.js
@@ -24,11 +24,21 @@ const LogInScreen = props => (
       />
     </View>
     <View style={styles.content}>
-      <TextInput placeholder="Username" />
-      <TextInput placeholder="Password" />
-      <TouchableOpacity>
-        <View>
-          <Text>Log In</Text>
+      <TextInput
+        placeholder="Username"
+        style={styles.textInput}
+        autoCapitalize={"none"}
+        autoCorrect={false}
+      />
+      <TextInput
+        placeholder="Password"
+        style={styles.textInput}
+        autoCapitalize={"none"}
+        secureTextEntry={true}
+      />
+      <TouchableOpacity style={styles.touchable}>
+        <View style={styles.button}>
+          <Text style={styles.btnText}>Log In</Text>
         </View>
       </TouchableOpacity>
       <TouchableOpacity style={styles.fbContainer}>
@@ -60,7 +70,7 @@ const styles = StyleSheet.create({
   content: {
     flex: 4,
     backgroundColor: "white",
-    paddingTop: 20,
+    paddingTop: 50,
     alignItems: "center",
     justifyContent: "flex-start"
   },
@@ -75,6 +85,34 @@ const styles = StyleSheet.create({
     color: "#3E99EE",
     marginLeft: 10,
     fontWeight: "600",
+    fontSize: 14
+  },
+  textInput: {
+    height: 50,
+    borderColor: "#bbb",
+    borderWidth: 1,
+    width: width - 80,
+    borderRadius: 5,
+    marginBottom: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FAFAFA",
+    fontSize: 14
+  },
+  touchable: {
+    borderRadius: 5,
+    backgroundColor: "#3E99EE",
+    width: width - 80,
+    marginTop: 25
+  },
+  button: {
+    paddingHorizontal: 7,
+    height: 50,
+    justifyContent: "center"
+  },
+  btnText: {
+    color: "white",
+    fontWeight: "600",
+    textAlign: "center",
     fontSize: 14
   }
 });
