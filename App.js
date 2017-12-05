b871aefcd7a67d039d18700c9b9a0934730cdf58
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-12-05 15:23:25 +0700

    #4-2 Persisting the Redux Store part Two

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React from "react";
 import { AppLoading, Asset, Font } from "expo";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
+import configureStore from "./redux/configureStore";
+const { persistor, store } = configureStore();
 
 class App extends React.Component {
   state = {
@@ -19,9 +23,13 @@ class App extends React.Component {
       );
     }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+          </View>
+        </PersistGate>
+      </Provider>
     );
   }
   _loadAssetsAsync = async () => {
