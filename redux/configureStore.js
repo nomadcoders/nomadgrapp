62307808f069d62f1fa68a7e02c44ee8c2d80001
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-12-05 15:22:56 +0700

    #4-2 Persisting the Redux Store part One

---
+++ redux/configureStore.js
@@ -0,0 +1,24 @@
+import { applyMiddleware, createStore } from "redux";
+import { persistStore, persistCombineReducers } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
+import user from "./modules/user";
+
+const middlewares = [thunk];
+
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const reducer = persistCombineReducers(persistConfig, {
+  user
+});
+
+const configureStore = () => {
+  let store = createStore(reducer, applyMiddleware(...middlewares));
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
+
+export default configureStore;
---
+++ redux/modules/user.js
@@ -0,0 +1,30 @@
+// Imports
+
+// Actions
+
+// Action Creators
+
+// API Actions
+
+// Initial State
+
+const initialState = {
+  isLoggedIn: false
+};
+
+// Reducer
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+// Reducer Functions
+
+// Exports
+
+// Default Reducer Export
+
+export default reducer;
